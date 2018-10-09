55867524ff43fb0b2293bdff4bf8c57c5ec72544
Martin <Martin@DESKTOP-1TL7PQ1>
2018-10-09 16:46:16 -0300

    Merge origin/master
    Conflicts:
    	nbproject/build-impl.xml
    	src/java/ControladorServlet/ServletAltaUsuario.java

--- src/logica/Interfaces/IPropCat.java
+++ src/logica/Interfaces/IPropCat.java
@@ -102,18 +102,19 @@ public interface IPropCat {
     public abstract List<DtNickTitProp> ListaEvaluarPropuesta();
 
     public abstract boolean EvaluarPropuesta(String titulo, TipoE tipo) throws Exception;
-
+    
     public abstract Usuario CargarFavoritas(Usuario usu, List<String> usufav);
-
+    
     public abstract List<DtinfoPropuesta> ListarPropuestasNoIngresadas(String nick);
-
+    
     public abstract List<DtinfoPropuesta> ListarPropuesta();
-
-    public abstract boolean AgregarFavorita(String titulo, String nick);
-
+    
+    public abstract boolean AgregarFavorita(String titulo,String nick);
+        
     public abstract List<DtinfoPropuesta> ListarPropuestaNOI();
 
     public abstract void EvaluarEstadosPropuestas();
-
+    
     public abstract List<DtPropuestaWeb> ListarPropuestasWeb(TipoE estado);
+
 }
